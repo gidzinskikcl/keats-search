0d211374909b06c5e089bc4d379328cc34cb09fa
gidzinskikcl <k24112096@kcl.ac.uk>
2025-06-11 22:37:01 +0100

    Add Transcript Schema and Extractor

--- src/data_collection/schemas.py
+++ src/data_collection/schemas.py
@@ -1,6 +1,8 @@
 from dataclasses import dataclass
 from typing import Optional
 
+from datetime import timedelta
+
 @dataclass
 class PdfPage:
     nr: int
@@ -16,6 +18,30 @@ class PdfSchema:
 class PdfSegment:
     parent_file: str
     nr: int
+    text: str
+
+@dataclass
+class Timestamp:
+    start: timedelta
+    end: timedelta
+
+@dataclass
+class Subtitle:
+    nr: int
+    text: str
+    timestamp: Timestamp
+
+@dataclass
+class TranscriptSchema:
+    file_name: str
+    duration: timedelta
+    subtitles: list[Subtitle]
+    course_name: Optional[str] = None
+
+@dataclass
+class TranscriptSegment:
+    parent_file: str
+    timestamp: Timestamp
     text: str
 
 
