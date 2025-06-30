0c76d486a7f0cd32877742901882e8ed3b8abcce
gidzinskikcl <k24112096@kcl.ac.uk>
2025-06-30 12:11:12 +0100

    Clean up Python files from lucene-search; keep only Java module

--- search_engines/lucene-search/src/data_collection/extractors/abstract_pdf_schema_extractor.py
+++
@@ -1,9 +0,0 @@
-import pathlib
-from abc import ABC, abstractmethod
-from data_collection import schemas
-
-class AbstractPdfSchemaExtractor(ABC):
-    @abstractmethod
-    def get(self, file_path: pathlib.Path) -> schemas.PdfSchema:
-        """Extracts a PdfSchema from a given PDF file path."""
-        pass
--- search_engines/lucene-search/src/data_collection/extractors/abstract_transcript_schema_extractor.py
+++
@@ -1,8 +0,0 @@
-import pathlib
-from abc import ABC, abstractmethod
-from data_collection import schemas
-
-class AbstractTranscriptSchemaExtractor(ABC):
-    @abstractmethod
-    def get(self, file_path: pathlib.Path) -> schemas.TranscriptSchema:
-        pass
